fecb8a37a292224d7d06e947b18ee359e8b44a72
240145 <240145@E0403-05>
2025-10-07 15:08:28 +0900

    GameObjectとRootJobを作成した

---
+++ Engine/GameObject.cpp
@@ -0,0 +1,19 @@
+#include "GameObject.h"
+
+GameObject::GameObject()
+	:pParent_(nullptr)
+{
+}
+
+GameObject::GameObject(GameObject* parent, const std::string& name)
+	:pParent_(parent),objectName_(name)
+{
+}
+
+GameObject::~GameObject()
+{
+}
+
+void GameObject::Initialize()
+{
+}
